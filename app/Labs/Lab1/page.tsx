303271cd5cf9ce55f13ce922335720a6058a5971
lilez <zhang.lile@northeastern.edu>
2025-09-06 23:01:06 -0400

    list, table, images

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -2,6 +2,171 @@ export default function Lab1() {
   return (
     <div id="wd-lab1">
       <h2>Lab 1</h2>
+      <h3>HTML Examples</h3>
+      <div id="wd-h-tag">
+        <h4>Heading Tags</h4>
+        Text documents are often broken up into several sections and
+        subsections. Each section is usually prefaced with a short title or
+        heading that attempts to summarize the topic of the section it precedes.
+        For instance this paragraph is preceded by the heading Heading Tags. The
+        font of the section headings are usually larger and bolder than their
+        subsection headings. This document uses headings to introduce topics
+        such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags
+        can be used to format plain text so that it renders in a browser as
+        large headings. There are 6 heading tags for different sizes: h1, h2,
+        h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest
+        heading.
+      </div>
+      <div id="wd-p-tag">
+        <h4>Paragraph Tag</h4>
+        <p id="wd-p-2">
+          This is the first paragraph. The paragraph tag is used to format
+          vertical gaps between long pieces of text like this one.
+        </p>
+        <p id="wd-p-3">
+          This is the second paragraph. Even though there is a deliberate white
+          gap between the paragraph above and this paragraph, by default
+          browsers render them as one contiguous piece of text as shown here on
+          the right.
+        </p>
+        <p id="wd-p-4">
+          This is the third paragraph. Wrap each paragraph with the paragraph
+          tag to tell browsers to render the gaps.
+        </p>
+      </div>
+      <div id="wd-lists">
+        <h4>List Tags</h4>
+        <h5>Ordered List Tag</h5>
+        How to make pancakes:
+        <ol id="wd-pancakes">
+          <li>Mix dry ingredients.</li>
+          <li>Add wet ingredients.</li>
+          <li>Stir to combine.</li>
+          <li>Heat a skillet or griddle.</li>
+          <li>Pour batter onto the skillet.</li>
+          <li>Cook until bubbly on top.</li>
+          <li>Flip and cook the other side.</li>
+          <li>Serve and enjoy!</li>
+        </ol>
+        My favorite recipe (steak):
+        <ol id="wd-your-favorite-recipe">
+          <li>Prepare the steak.</li>
+          <li>Heat the pan.</li>
+          <li>Sear the steak.</li>
+          <li>Add aromatics and baste.</li>
+          <li>Finish cooking to your desired doneness.</li>
+          <li>Rest the steak.</li>
+        </ol>
+        <h5>Unordered List Tag</h5>
+        My favorite books (in no particular order)
+        <ul id="wd-my-books">
+          <li>Dune</li>
+          <li>Lord of the Rings</li>
+          <li>Ender's Game</li>
+          <li>Red Mars</li>
+          <li>The Forever War</li>
+        </ul>
+        Your favorite books (in no particular order)
+        <ul id="wd-your-books">
+          <li>The Kite Runner</li>
+          <li>Harry Potter</li>
+          <li>War and Peace</li>
+        </ul>
+      </div>
+      <div id="wd-tables">
+        <h4>Table Tag</h4>
+        <table border={1} width="100%">
+          <thead>
+            <tr>
+              <th>Quiz</th>
+              <th>Topic</th>
+              <th>Date</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Q1</td>
+              <td>HTML</td>
+              <td>2/3/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q2</td>
+              <td>CSS</td>
+              <td>2/10/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q3</td>
+              <td>CSS2</td>
+              <td>2/15/21</td>
+              <td>95</td>
+            </tr>
+            <tr>
+              <td>Q4</td>
+              <td>DB</td>
+              <td>2/18/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q5</td>
+              <td>GUI</td>
+              <td>2/21/21</td>
+              <td>80</td>
+            </tr>
+            <tr>
+              <td>Q6</td>
+              <td>ALGO</td>
+              <td>2/24/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q7</td>
+              <td>GRAPH</td>
+              <td>2/27/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q8</td>
+              <td>REACT</td>
+              <td>3/1/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q9</td>
+              <td>JS</td>
+              <td>3/8/21</td>
+              <td>95</td>
+            </tr>
+            <tr>
+              <td>Q10</td>
+              <td>FINAL</td>
+              <td>3/9/21</td>
+              <td>95</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>Average</td>
+              <td>90</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <div id="wd-images">
+        <h4>Image tag</h4>
+        Loading an image from the internet: <br />
+        <img
+          id="wd-starship"
+          width="400px"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <br />
+        Loading a local image:
+        <br />
+        <img id="wd-teslabot" src="/images/teslabot.jpeg" height="200px" />
+      </div>
     </div>
-);
+  );
 }
